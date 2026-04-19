c57bb35098a565498f5465881ce1dbd09ec3e7df
José Luis González Rojas <joseluisgonzalezrojas@pm.me>
2026-04-19 14:48:09 -0600

    a7_ReglasNombresVariables del curso de Universidad Java en Udemy

---
+++ udemy/universidad_java/a7_ReglasNombresVariables/src/ReglasNombresVariables.java
@@ -0,0 +1,18 @@
+public class ReglasNombresVariables {
+    static void main() {
+        String nombreCompleto = "Juan Carlos";
+        System.out.println("nombreCompleto = " + nombreCompleto);
+        String NombreCompleto = "Juan Carlos 2";
+        System.out.println("NombreCompleto = " + NombreCompleto);
+        String nombre_cliente = "Juan";
+        String _apellido = "Pérez";
+        String $apellido = "Juárez";
+        int totPzs = 10;
+        int totalPiezas = 10;
+        boolean casado = true;
+        boolean esCasado = true;
+        boolean isCasado = true;
+        boolean tieneSaldo = true;
+        boolean hasSaldo = true;
+    }
+}
