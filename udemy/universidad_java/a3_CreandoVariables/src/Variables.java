7d8b9c8783d3b0b3c050eaa2c5db5dedc4c2ddbe
José Luis González Rojas <joseluisgonzalezrojas@pm.me>
2026-04-19 14:04:51 -0600

    a3_CreandoVariables del curso de Universidad Java en Udemy

---
+++ udemy/universidad_java/a3_CreandoVariables/src/Variables.java
@@ -0,0 +1,14 @@
+void main() {
+    int edad = 31;
+    double precio = 300.40;
+    boolean disponible = true;
+    char genero;
+    genero = 'M';
+
+    IO.println(edad);
+    edad = 35;
+    IO.println(edad);
+    IO.println(precio);
+    IO.println(disponible);
+    IO.println(genero);
+}
