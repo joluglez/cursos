64a7ac25b16c15afeedefbf780aeac305d99a8e7
José Luis González Rojas <joseluisgonzalezrojas@pm.me>
2026-04-19 14:26:17 -0600

    a5_DetallePersona del curso de Universidad Java en Udemy

---
+++ udemy/universidad_java/a5_DetallePersona/src/DetallePersona.java
@@ -0,0 +1,13 @@
+void main() {
+    String nombreCompleto = "José Luis González Rojas";
+    int edad = 24;
+    float altura = 1.73f;
+    String paisOrigen = "México";
+    char estadoCivil = 'S';
+
+    IO.println(nombreCompleto);
+    IO.println(edad);
+    IO.println(altura);
+    IO.println(paisOrigen);
+    IO.println(estadoCivil);
+}
