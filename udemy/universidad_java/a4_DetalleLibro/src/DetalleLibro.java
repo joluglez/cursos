dbb78a2f921225577a7e70c21ff2f20842287a3a
José Luis González Rojas <joseluisgonzalezrojas@pm.me>
2026-04-19 14:20:36 -0600

    a4_DetalleLibro del curso de Universidad Java en Udemy

---
+++ udemy/universidad_java/a4_DetalleLibro/src/DetalleLibro.java
@@ -0,0 +1,15 @@
+void main() {
+    String tituloLibro = "El señor de los anillos";
+    int anioPublicacion = 1954;
+    boolean libroDisponible = true;
+    double precio = 500.50;
+
+    IO.println(tituloLibro);
+    IO.println(anioPublicacion);
+    IO.println(libroDisponible);
+    IO.println(precio);
+
+    tituloLibro = "El Señor de dos Anillos";
+
+    IO.println(tituloLibro);
+}
